97c1420c05f8025ccdeaf3346babd3abee1091bc
manuel-magana-dev <magana.m@northeastern.edu>
2026-03-10 07:26:01 -0700

    feat: add health check endpoint and tests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,16 @@
 """This is the main entry point for the FastAPI application.
 It sets up the application and configures CORS middleware to allow requests from specified origins.
+
+Routers:
+- health: Provides a simple health check endpoint to 
+          verify that the API is running.
+          
 """
 from fastapi import FastAPI
 from .middleware.cors import add_cors_middleware
+from .routers import health
+
+
 app = FastAPI()
 add_cors_middleware(app)
 
@@ -10,3 +18,4 @@ add_cors_middleware(app)
 def root():
     """A simple root endpoint to verify that the API is running."""
     return {"app": "WhatToDo API"}
+app.include_router(health.router)
